9166fe7337b12a2afe68ace14dd756fb58b22e31
Max Estrella <maxestrella565@gmail.com>
2025-03-10 21:20:34 +0000

    Form finished

--- quote.php
+++ quote.php
@@ -7,6 +7,7 @@ require 'Composer/vendor/autoload.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
+    $date = isset($_POST['date']) ? implode(" to ", $_POST['date']) : "No dates selected";
     $services = isset($_POST['services']) ? implode(", ", $_POST['services']) : "No services selected";
     $message = htmlspecialchars($_POST['message']);
 
@@ -17,16 +18,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = ''; // Add Email
-        $mail->Password = ''; // Add App Password
+        $mail->Username = 'saidaihoshi@gmail.com'; // Add Email
+        $mail->Password = 'hiqm yraq blct alxw'; // Add App Password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;  
 
         // Email Setup
         $mail->setFrom($email, $name);
-        $mail->addAddress('', 'Business Owner'); // Add Email
+        $mail->addAddress('saidaihoshi@gmail.com', 'Business Owner'); // Add Email
         $mail->Subject = 'New Quote Request';
-        $mail->Body = "Name: $name\nEmail: $email\nServices: $services\nMessage: $message";
+        $mail->Body = "Name: $name\nEmail: $email\nDates: $date\nServices: $services\nMessage: $message";
 
         $mail->send();
         echo "Email sent successfully!";
